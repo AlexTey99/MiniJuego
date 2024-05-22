36f7bbb38809109d9c8ad89612c343cb94920f66
AlexTey99 <teyalex99@gmail.com>
2024-05-22 20:56:36 +0200

    Se an agregado los enemigos y se an echo sus animaciones

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const gameOver = document.getElementById('gameOver');
 let distancia = 0;
 const incremento = 10;
 
+// Configuracion del airPlane.
+
+// Desplazamiento al precionar las teclas arrowLeft/Rigth.
 document.addEventListener('keydown', (event) => {
     if(event.key === 'ArrowRight'){
         distancia += incremento;
@@ -16,15 +19,20 @@ document.addEventListener('keydown', (event) => {
     }
 });
 
+
+// Configuracion de enemies.
+
+// Desplazamiento de enemies y resultado final de la colicion.
 window.addEventListener('load', () => {
     enemies.classList.add('fall');
 
+    // Colicion.
     function detectarColision(elemento1, elemento2) {
-        // Obtener las posiciones de los elementos
+        // Obtiene las posiciones de los elementos
         const rect1 = elemento1.getBoundingClientRect();
         const rect2 = elemento2.getBoundingClientRect();
 
-        // Comprobar si los rectángulos de los elementos se superponen
+        // Comprueba si los rectángulos de los elementos se superponen
         return !(
             rect1.right < rect2.left ||
             rect1.left > rect2.right ||
@@ -32,17 +40,65 @@ window.addEventListener('load', () => {
             rect1.top > rect2.bottom
         );
     }
-
-    setInterval(() => {
+    
+    function verificarColision() {
         if (detectarColision(airPlane, enemies)) {
-            console.log('¡Los elementos están colisionando!');
             airPlane.style.display = 'none';
             enemies.style.display = 'none';
             gameOver.style.display = 'flex';
-
+        } else {
+            requestAnimationFrame(verificarColision);
         }
-    }, 100);
-})
+    }
+
+    verificarColision();
+});
+
+
+
+
+
+
+
+
+
+window.onload = function() {
+    setInterval(function() {
+        createFallingDiv();
+    }, 1000);
+};
+
+function createFallingDiv() {
+    var windowWidth = window.innerWidth;
+    var divWidth = 50; // Ancho del div
+    var maxLeft = windowWidth - divWidth;
+    var randomLeft = Math.floor(Math.random() * maxLeft);
+
+    var div = document.createElement('div');
+    div.className = 'enemies';
+    div.style.left = randomLeft + 'px';
+    document.body.appendChild(div);
+
+    animateFallingDiv(div);
+}
+
+function animateFallingDiv(div) {
+    var windowHeight = window.innerHeight;
+    var duration = Math.floor(Math.random() * 3000) + 2000; // Duración de la animación entre 2 y 5 segundos
+
+    var animation = div.animate([
+        { top: '0', opacity: 0 },
+        { top: windowHeight + 'px', opacity: 1 }
+    ], {
+        duration: duration,
+        easing: 'linear'
+    });
+
+    animation.onfinish = function() {
+        div.remove();
+    };
+}
+
 
 
     
